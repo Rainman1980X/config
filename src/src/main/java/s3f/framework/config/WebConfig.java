d226923ecadca5f1e74b961d73ff10399fd52394
Matthias Burger <matthias.burger@sintec.de>
2016-09-23 13:53:09 +0200

    webconfig die 2.

---
+++ src/src/main/java/s3f/framework/config/WebConfig.java
@@ -0,0 +1,19 @@
+package s3f.framework.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by MSBurger on 23.09.2016.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**");
+    }
+}
