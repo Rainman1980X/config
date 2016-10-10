956b32fbd159ea2724dfc137beb18ca77e33a884
Matthias Burger <matthias.burger@sintec.de>
2016-10-10 18:21:34 +0200

    add tests

--- src/src/test/java/s3f/s3f_configuration/controller/S3FConfigurationConstantControllerTest.java
+++ src/src/test/java/s3f/s3f_configuration/controller/S3FConfigurationConstantControllerTest.java
@@ -16,6 +16,8 @@ import s3f.s3f_configuration.dto.S3FConfigurationConstantDto;
 import s3f.s3f_configuration.services.S3FConfigurationConstantService;
 
 public class S3FConfigurationConstantControllerTest {
+    private final String authorization = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIiLCJyb2xlIjoiYWRtaW4iLCJuYmYiOjE0NzQ2Mzc4NjIsImlzcyI6InMzZiIsImV4cCI6NzI4OTU2NDQwMH0.SDuVE3Eg8uV7JXWrrjQAw7c_2NeD4fe7EwjCX4ULZto";
+    private final String correlationToken = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIiLCJyb2xlIjoiYWRtaW4iLCJuYmYiOjE0NzQ2Mzc4NjIsImlzcyI6InMzZiIsImV4cCI6NzI4OTU2NDQwMH0.SDuVE3Eg8uV7JXWrrjQAw7c_2NeD4fe7EwjCX4ULZto";
     private final String id = "test_2908233490284ß902842390";
     private final String version = "v1";
     private final String lifecycle = "develop";
@@ -32,7 +34,8 @@ public class S3FConfigurationConstantControllerTest {
     @Test
     public void post() throws Exception {
         final S3FConfigurationConstantDto s3FConfigurationConstantDto = s3FConfigurationConstantDto();
-        ResponseEntity responseEntity = s3FConfigurationConstantController.create(s3FConfigurationConstantDto);
+	ResponseEntity responseEntity = s3FConfigurationConstantController.create(authorization, correlationToken,
+		s3FConfigurationConstantDto);
 
         assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));
         verify(s3FConfigurationConstantService).create(s3FConfigurationConstantDto);
@@ -42,14 +45,16 @@ public class S3FConfigurationConstantControllerTest {
     public void get() throws Exception {
         final S3FConfigurationConstantDto s3FConfigurationConstant = s3FConfigurationConstant();
         when(s3FConfigurationConstantService.read("version", "lifecycle","encMongoDBHost")).thenReturn(s3FConfigurationConstant);
-        ResponseEntity responseEntity = s3FConfigurationConstantController.read("version", "lifecycle","encMongoDBHost");
+	ResponseEntity responseEntity = s3FConfigurationConstantController.read(authorization, correlationToken,
+		"version", "lifecycle", "encMongoDBHost");
 
         assertThat(responseEntity.getBody(), is(s3FConfigurationConstant));
     }
 
     @Test
     public void put() throws Exception {
-        ResponseEntity responseEntity = s3FConfigurationConstantController.update(s3FConfigurationConstant());
+	ResponseEntity responseEntity = s3FConfigurationConstantController.update(authorization, correlationToken,
+		s3FConfigurationConstant());
 
         assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));
         verify(s3FConfigurationConstantService).update(s3FConfigurationConstant());
